8e7719164c997f3e928c6a2f73e60388f8bc206a
Ashwin1934 <ashud219@gmail.com>
2020-06-19 14:53:39 -0400

    Establish connection between python and mysql DB

--- ContinuousIntegrationPlayground.py
+++ ContinuousIntegrationPlayground.py
@@ -4,4 +4,11 @@ Created on Fri Jun 19 12:30:39 2020
 
 @author: ashud
 """
+import sqlalchemy
+import pymysql
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+database_URI = 'mysql+pymysql://@localhost:3306/MyDatabase'
+engine = create_engine(database_URI, echo=True)
