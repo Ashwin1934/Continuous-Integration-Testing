528f4e1299c9ac3a466140abb4c081f0b44ee218
Ashwin1934 <ashud219@gmail.com>
2020-06-26 12:46:07 -0400

    Working on generalizing tests for all cases/ adding tests for individual table add/drop

--- Manager.py
+++ Manager.py
@@ -25,9 +25,22 @@ class DBManager:
         self.engine = create_engine(URI, echo=True)
         #Session = sessionmaker(bind=self.engine)
         #self.session = Session()
+        
     def addAllTables(self):
-        print('TEST')
         CI.base.metadata.create_all(self.engine)
+        
+    def print_tables_from_metadata(self):
+        #prints out dictionary of all the tables in CI.py
+        #print(CI.base.metadata.tables)
+        #print(CI.Course.__tablename__)
+        #use this method to make a list of just the table names for the unittests
+        lisT = []
+        for key in CI.base.metadata.tables:
+            lisT.append(key)
+        return lisT
+    
+    def addSingleTable(self, table):
+        CI.base.metadata.create_all(self.engine, tables = [table])
         
     def dropSingleTable(self, tablename):
         table = CI.base.metadata.tables.get(tablename)
@@ -35,6 +48,16 @@ class DBManager:
         
     def dropAllTables(self):
         CI.base.metadata.drop_all(self.engine)
+        
+
+database_URI = 'mysql+pymysql://root:@localhost:3306/MyDatabase'
+db = DBManager(database_URI)
+temp = db.print_tables_from_metadata()
+#print(temp)
+#db.addSingleTable(CI.Course.__table__)
+
+        
+
         
 
 
--- TestTravis.py
+++ TestTravis.py
@@ -8,28 +8,58 @@ Created on Fri Jun 19 12:30:45 2020
 
 import unittest
 import Manager
+from CI import Course
 
 emptylist = ['alembic_version', 'stockprices']
 fulllist = ['alembic_version', 'courses', 'stockprices', 'students']
 
 class Tests(unittest.TestCase):
     
+    """
+      Test for adding all ORM tables. The resultant list should have every table
+      in CI.py and any existing tables on the MySQL db. 
+    """
     
     def test_add_all_tables(self):
         db_uri = 'mysql+pymysql://root:@localhost:3306/MyDatabase'
         dbm = Manager.DBManager(db_uri)
+        dbm.dropAllTables()
+        before = dbm.engine.table_names()
         dbm.addAllTables()
-        result = dbm.engine.table_names()
-        self.assertEqual(result, fulllist)
-        
+        after = dbm.engine.table_names()
+        #this is a list of all tables in CI.py
+        metadatatables = dbm.print_tables_from_metadata
+        expected = metadatatables.copy()
+        expected.update(before)
+        print(expected)
+        print(after)
+        self.assertEqual(after, expected)
+    
+    @unittest.skip("Temporary")
     def test_drop_all_tables(self):
        db_uri = 'mysql+pymysql://root:@localhost:3306/MyDatabase'
        dbm = Manager.DBManager(db_uri)
        dbm.dropAllTables()
        result = dbm.engine.table_names()
        self.assertEqual(result, emptylist)
+    @unittest.skip("Temporary")
+    def test_add_single_table(self):
+        db_uri = 'mysql+pymysql://root:@localhost:3306/MyDatabase'
+        dbm = Manager.DBManager(db_uri)
+        dbm.addSingleTable(Course.__table__)
+        listtables = dbm.engine.table_names()
+        self.assertIn(Course.__tablename__, listtables)
+    
+    @unittest.skip("temp")
+    def test_drop_single_table(self):
+        db_uri = 'mysql+pymysql://root:@localhost:3306/MyDatabase'
+        dbm = Manager.DBManager(db_uri)
+        dbm.dropSingleTable(Course.__tablename__)
+        result = dbm.engine.table_names()
+        self.assertEqual(result, emptylist)
+
+        
        
-    def test_add_S
        
         
         
